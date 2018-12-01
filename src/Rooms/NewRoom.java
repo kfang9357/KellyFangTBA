37d523366b7e3c2ad1623cdce1877a2baebc6d74
kfang9357 <43207499+kfang9357@users.noreply.github.com>
2018-12-01 00:38:45 -0500

    1

---
+++ src/Rooms/NewRoom.java
@@ -0,0 +1,20 @@
+package Rooms;
+
+import Game.Runner;
+import People.Person;
+
+public class NewRoom extends Room {
+
+    public NewRoom (int x, int y){
+        super(x,y);
+    }
+
+    @Override
+    public void enterRoom(Person x) {
+
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println(".");
+    }
+}
